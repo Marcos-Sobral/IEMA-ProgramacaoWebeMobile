aff7a64eda79ce50f7bd255ea7bc226708695938
Marcos-Sobral <vini15oficial@gmail.com>
2025-11-11 15:20:45 -0300

    ✨ feat: Material de Desvio de Condicional

---
+++ ATIVIDADE 04_Bootstrap e JavaScript/DesvioCondicional/04 - DesvioMultiplaEscolha/script.js
@@ -0,0 +1,15 @@
+let fruta = "maçã";
+
+switch (fruta) {
+  case "banana":
+    console.log("Você escolheu banana");
+    break;
+  case "maçã":
+    console.log("Você escolheu maçã");
+    break;
+  case "laranja":
+    console.log("Você escolheu laranja");
+    break;
+  default:
+    console.log("Fruta não reconhecida");
+}
