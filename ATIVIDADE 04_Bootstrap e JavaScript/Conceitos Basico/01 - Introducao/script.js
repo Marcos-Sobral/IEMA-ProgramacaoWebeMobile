49e063675495dc1f9d13055353c42b3833929212
Marcos-Sobral <vini15oficial@gmail.com>
2025-11-11 14:49:15 -0300

    ✨ feat: Material de Escopo de variavel

---
+++ ATIVIDADE 04_Bootstrap e JavaScript/Conceitos Basico/01 - Introducao/script.js
@@ -0,0 +1,10 @@
+alert("Olá, Mundo!");
+
+let nome_usuario = prompt("Qual é o seu nome ?");
+alert("O usuario digitou: "+nome_usuario);
+//console.log("O nome do usuário é: "+nome_usuario);
+
+let preco = 150;
+console.log("O preço inicial é: ", preco);
+preco = preco * 1.1;
+console.log("O novo preço é: ", preco)
